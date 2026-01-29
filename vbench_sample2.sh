ec331d4ad1d907c63ed0bd783ced4113c4d750d0
Hang Guo <1911610@mail.nankai.edu.cn>
2026-01-29 14:19:07 +0800

    Delete vbench_sample2.sh

--- vbench_sample2.sh
+++
@@ -1,7 +0,0 @@
-export CUDA_VISIBLE_DEVICES=1
-torchrun --nproc_per_node=1 --master_port=28500 sample_vbench.py --config_path configs/longlive_vbench.yaml
-
-cd ..
-cd ./VBench
-bash ada_evaluate.sh /home/hangguo/dataset/20260107_single5s_longlive_teacache_ours /home/hangguo/VBench/sample_vbench/20260107_single5s_longlive_teacache_ours
-
